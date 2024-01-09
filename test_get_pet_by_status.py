87931a29a3366086ae1159bf3ff1576d10c00375
andrew1andrew <andrey-zheleznyak124@yandex.ru>
2024-01-09 11:43:47 +0300

    add gitignore

--- test_get_pet_by_status.py
+++ test_get_pet_by_status.py
@@ -6,7 +6,6 @@ def get_pet(status):
     url = "https://petstore.swagger.io/v2/pet/findByStatus"
     data = {"status": status}
     response = requests.get(url, params=data)
-    print(response.json())
     assert response.status_code == 200, "Wrong response code"
     for pet_data in response.json():
         # assert "name" in pet_data and pet_data["name"] == "doggie", "No main value for key status"
