e759f76d66ab1e63221ee9c168066f124cf32c36
Andrew <andrey-zheleznyak124@yandex.ru>
2024-01-09 09:18:22 +0300

    add three methods for the pet by status

--- test_get_pet_by_status.py
+++ test_get_pet_by_status.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 
@@ -5,10 +6,21 @@ def get_pet(status):
     url = "https://petstore.swagger.io/v2/pet/findByStatus"
     data = {"status": status}
     response = requests.get(url, params=data)
+    print(response.json())
     assert response.status_code == 200, "Wrong response code"
-    assert "category" in response.json(), "No main value for key status"
+    for pet_data in response.json():
+        # assert "name" in pet_data and pet_data["name"] == "doggie", "No main value for key status"
+        assert "status" in pet_data and pet_data["status"] == status
 
-
+#@pytest.mark.
 class TestGetPetByStatus:
     def test_get_pet_available(self):
         get_pet("available")
+
+    def test_get_pet_pending(self):
+        get_pet("pending")
+
+    def test_get_pet_sold(self):
+        get_pet("sold")
+
+
